b6157ee9c116c64fa44aa1fb951969214c968dc4
David <43955404+236807@users.noreply.github.com>
2019-05-06 11:47:21 +0200

    Add files via upload

---
+++ createSubstitudeMatrix.m
@@ -0,0 +1,17 @@
+function [str] = createSubstitudeMatrix(seq1,seq2,score)
+str{1} = 'X';
+str{1,2} = ' ';
+str{2,1} = ' ';
+str{2,2} = 0;
+length(seq1);
+for i=2 : length(seq1)+1
+    for j=2 : length(seq2)+1
+         str{1,j} = seq2(j-1); % Ustawienie pierwszego wiersza na ACGTU lub AGCT
+         str{i,j} = (score(i-1,j-1));      
+    end
+   str{i,1} = seq1(i-1); % Ustawienie pierwszej kolumny na ACGTU lub AGCT   
+end
+end
+
+
+
